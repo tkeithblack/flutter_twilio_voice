6a321ebd92ca7508e58fe5f53861d2cdfe36e4c0
Keith Black <tkeithblack@gmail.com>
2021-06-21 14:00:17 -0500

    Removed FULLSCREEN flag to prevent status bar from being obscured.

--- android/src/main/java/com/dormmom/flutter_twilio_voice/FlutterTwilioVoicePlugin.java
+++ android/src/main/java/com/dormmom/flutter_twilio_voice/FlutterTwilioVoicePlugin.java
@@ -258,7 +258,7 @@ public class FlutterTwilioVoicePlugin implements FlutterPlugin, MethodChannel.Me
         Window window = activity.getWindow();
 
         window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-                WindowManager.LayoutParams.FLAG_FULLSCREEN |
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN |
                         WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
                         WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
                         WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
